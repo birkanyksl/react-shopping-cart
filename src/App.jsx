a4c58684f0a9cab404e6eaa23959010402ec239d
Birkan <birkanyksl613@gmail.com>
2024-03-26 18:52:11 +0300

    Ürün divleri oluşturuldu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Products from "./components/Products";
 
 function App() {
   return (
     <div className="container mx-auto p-4">
       <Header />
+      <Products />
     </div>
   );
 }
