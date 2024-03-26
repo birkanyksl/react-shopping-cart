9de9a20ca0ac97f712393550a39bb8242aea243e
Birkan <birkanyksl613@gmail.com>
2024-03-26 23:47:13 +0300

    sepet işlemleri eklendi.
    ekleme, çıkartma, total fiyat

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,26 @@
-const Cart = () => {
+/* eslint-disable react/prop-types */
+
+const Cart = ({ cart, emptyCart }) => {
+  const total = cart.reduce((acc, item) => acc + item.price, 0);
+
+  if (cart.length === 0) return;
   return (
     <div className="border ml-auto w-72 p-4 mt-2 rounded-lg shadow-lg">
       <h2 className="text-2xl font-semibold mb-4">Sepet</h2>
       <ul>
-        <li className="mt-2 flex justify-between">
-          <span>Ürüm 1</span>
-          <span>100 TL</span>
-        </li>
+        {cart.map((item, index) => (
+          <li className="mt-2 flex justify-between" key={index}>
+            <span>{item.name}</span>
+            <span>{item.price}</span>
+          </li>
+        ))}
       </ul>
       <hr className="my-4" />
-      <p className="font-semibold text-xl">Toplam: 100TL</p>
-      <button className="bg-red-500 text-white px-4 py-2 rounded w-full hover:bg-red-700 transition-all duration-200 mt-4">
+      <p className="font-semibold text-xl">Toplam: {total} TL</p>
+      <button
+        className="bg-red-500 text-white px-4 py-2 rounded w-full hover:bg-red-700 transition-all duration-200 mt-4"
+        onClick={emptyCart}
+      >
         Sepeti Boşalt
       </button>
     </div>
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,10 @@
 import { ProductItemPropTypes } from "../propTypes";
 
-const ProductItem = ({ product }) => {
+const ProductItem = ({ product, setCart }) => {
+  const addToCart = (product) => {
+    setCart((prevState) => [...prevState, product]);
+  };
+
   return (
     <div className="border p-4 m-2 rounded-lg">
       <img
@@ -11,7 +15,10 @@ const ProductItem = ({ product }) => {
       <div className="p-4">
         <h2 className="text-xl font-semibold ">{product.name}</h2>
         <p className="text-gray-500 my-2">{product.price} TL</p>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full">
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full"
+          onClick={() => addToCart(product)}
+        >
           Sepete Ekle
         </button>
       </div>
